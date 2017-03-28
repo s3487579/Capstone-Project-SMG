35a0aa4ca56aa2918f932e59eaa62bb2ec89db30
shammymcrammy <saenero20@gmail.com>
2017-03-28 05:53:11 +0000

    Added page routing and some sample pages

--- myApp/both/router/routes.js
+++ myApp/both/router/routes.js
@@ -1,6 +1,11 @@
-    FlowRouter.route('/', {
-        name: 'main',
-        function() {
-            BlazeLayout.render('main')
-        }
-    });
+FlowRouter.route('/',{
+    action:function(){
+        FlowLayout.render('layout', {sidebar: 'sidebar', home: 'home', cart: 'cart'})
+    }
+});
+
+FlowRouter.route('/sharemarket',{
+    action:function(){
+        FlowLayout.render('layout', {sidebar: 'sidebar', home: 'sharemarket', cart: 'cart'})
+    }
+});
